899fc4ea5f9b2a0c8b6e4455631b8d2efd84415d
joao.lucas <joao.lucas@14mob.com>
2022-05-12 15:08:25 -0300

    criando formulario de atualizacao

--- src/Tabela.js
+++ src/Tabela.js
@@ -2,10 +2,12 @@ import { getDefaultNormalizer } from "@testing-library/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+
+
 function Tabela(){
 
     const [usuarios, setUsuarios] = useState([]);
-
+    const [id,setId] = useState("");
     const [nome,setNome] = useState("");
     const [email,setEmail] = useState("");
     const [senha,setSenha] = useState("");
@@ -13,6 +15,7 @@ function Tabela(){
     
     function salvarFormulario(){
         
+
         let parametros = {
             name: nome,
             email: email,
@@ -27,6 +30,41 @@ function Tabela(){
         }).catch( error => console.log(error));
 
     }
+
+    function removerUsuario(id){
+        console.log('funcionando' + id);
+
+        axios.delete("https://iot.14mob.com/api-fiap/public/index.php/users/" + id).then( response => {
+            alert('Deu certo removi o usuario')
+
+            window.location = "";
+
+        }).catch( error => console.log(error));
+
+    }
+
+    function atualizarUsuarioApi(){
+
+        let parametros = {
+            name: nome,
+            email: email,
+            password: senha
+        }
+        axios.put('https://iot.14mob.com/api-fiap/public/index.php/users/'+ id, parametros).then(response => {
+            if(response.status == 200){
+                alert('Ebaaaaa deu certo')
+            }else{
+                alert('lascou')
+            }
+        }).catch( error => console.log(error));
+    }
+
+    function atualizarUsuario(usuario){
+        setId(usuario.id);
+        setNome(usuario.name);
+        setEmail(usuario.email);
+        setSenha(usuario.password);
+    }
     
   useEffect(() => {
         axios.get('https://iot.14mob.com/api-fiap/public/index.php/users').then( response => {
@@ -40,15 +78,19 @@ function Tabela(){
       <div>
         <form className="formulario" onSubmit={event => {
             event.preventDefault();
-            salvarFormulario();
+            if(id != ''){
+                atualizarUsuarioApi()
+            }else{
+                salvarFormulario();
+            }
             return false;
         } } > 
         <label>Nome</label>
-        <input name="name"  onChange={ e => setNome(e.target.value) } />
+        <input name="name" value={ nome }  onChange={ e => setNome(e.target.value) } />
         <label>Email</label>
-        <input name="email" onChange={ e => setEmail(e.target.value) } />
+        <input name="email" value={ email } onChange={ e => setEmail(e.target.value) } />
         <label>senha</label>
-        <input name="password" onChange={ e => setSenha(e.target.value) } />
+        <input name="password" value={ senha } onChange={ e => setSenha(e.target.value) } />
         
         <button type="submit">Enviar</button>
         </form>
@@ -63,6 +105,7 @@ function Tabela(){
                 <th>ID</th>
                 <th>Nome</th>
                 <th>Email</th>
+                <th>Ações</th>
                 </tr>
             
             </thead>  
@@ -73,6 +116,11 @@ function Tabela(){
                             <td>{usuario.id}</td>
                             <td>{usuario.name}</td>
                             <td>{usuario.email}</td>
+                            <td>
+                                <button onClick={ event => atualizarUsuario(usuario) } > Editar </button>
+                                <button onClick={ event => removerUsuario(usuario.id) } > Deletar </button>
+                            </td>
+                            
                         </tr>
                         )
                 } ) }
