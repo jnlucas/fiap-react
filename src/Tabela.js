82d9cb0692ce4ffaf9d62c4faaa00bd6c348b058
joao.lucas <joao.lucas@14mob.com>
2022-05-12 11:25:24 -0300

    criando chamada http com metodo post

--- src/Tabela.js
+++ src/Tabela.js
@@ -1,12 +1,33 @@
+import { getDefaultNormalizer } from "@testing-library/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-
 
 function Tabela(){
 
     const [usuarios, setUsuarios] = useState([]);
 
+    const [nome,setNome] = useState("");
+    const [email,setEmail] = useState("");
+    const [senha,setSenha] = useState("");
 
+    
+    function salvarFormulario(){
+        
+        let parametros = {
+            name: nome,
+            email: email,
+            password: senha
+        }
+        axios.post('https://iot.14mob.com/api-fiap/public/index.php/users', parametros).then(response => {
+            if(response.status == 201){
+                alert('Ebaaaaa deu certo')
+            }else{
+                alert('lascou')
+            }
+        }).catch( error => console.log(error));
+
+    }
+    
   useEffect(() => {
         axios.get('https://iot.14mob.com/api-fiap/public/index.php/users').then( response => {
             setUsuarios(response.data.users);
@@ -17,16 +38,25 @@ function Tabela(){
 
   return (
       <div>
-        <form className="formulario">
+        <form className="formulario" onSubmit={event => {
+            event.preventDefault();
+            salvarFormulario();
+            return false;
+        } } > 
         <label>Nome</label>
-        <input name="name" value="" />
+        <input name="name"  onChange={ e => setNome(e.target.value) } />
         <label>Email</label>
-        <input name="email" value="" />
+        <input name="email" onChange={ e => setEmail(e.target.value) } />
         <label>senha</label>
-        <input name="password" value="" />
+        <input name="password" onChange={ e => setSenha(e.target.value) } />
         
         <button type="submit">Enviar</button>
         </form>
+
+        <p>{ nome }</p>
+        <p>{ email }</p>
+        <p>{ senha }</p>
+        
         <table className='minhaTabela'>
             <thead>
                 <tr>
